f43737d6a3fba2d0aa5842dc13f356ca5f53d6b8
acreator57887 <absurfer57887@gmail.com>
2018-06-13 15:51:29 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo "<pre>";
+print_r($_GET);
+echo "</pre><br>";
 
 $access_token = $_GET["token"];
 $api = 'https://api.telegram.org/bot' . $access_token;
