0725aa03405cf9c6b74920400aced147fe36455c
acreator57887 <absurfer57887@gmail.com>
2018-06-13 15:45:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
 <?php
 
-echo 'Hello World!';
+$access_token = $_GET["token"]
+$api = 'https://api.telegram.org/bot' . $access_token;
+
+
+function sendMessage($chat_id, $text, $reply_markup = false, $parse_mode = false, $preview = false) {
+	global $api;
+	
+	$message = ['chat_id' => $chat_id, 'text' => $text, 'disable_web_page_preview' => true];
+	if ($reply_markup !== false) $message["reply_markup"] = json_encode($reply_markup);
+	if ($parse_mode !== false) $message["parse_mode"] = $parse_mode;
+	
+	if ($preview) unset($message['disable_web_page_preview']);
+
+	$postdata = http_build_query($message);
+
+	$opts = ['http' =>
+	    [
+	        'method'  => 'POST',
+	        'header'  => 'Content-type: application/x-www-form-urlencoded',
+	        'timeout' => 1,
+	        'content' => $postdata
+	    ]
+	];
+
+	$context  = stream_context_create($opts);
+
+	$result = @file_get_contents("$api/sendMessage", false, $context);
+}
+
+sendMessage($_GET["to"], $_GET["text"]);
+
+?>
